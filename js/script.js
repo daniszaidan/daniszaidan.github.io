29360674178ac969391ed5778399b4aaa14bdbdf
Danis Zaidan <daniszaidan@gmail.com>
2019-06-15 18:15:47 +0700

    test onload function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+window.onload = function () {
+    console.log("sudah berhenti loading");
+};
+
 var buttons = document.getElementsByTagName('button');
 Array.prototype.forEach.call(buttons, function (b) {
     b.addEventListener('click', createRipple);
