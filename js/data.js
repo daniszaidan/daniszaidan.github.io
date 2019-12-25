35e925be42a3705abe72e1b40436331d2b2e251d
Danis Zaidan <daniszaidan@gmail.com>
2019-12-25 14:07:59 +0700

    try using json to display portfolio data

---
+++ js/data.js
@@ -0,0 +1,124 @@
+let portfolio_data = {
+    key_1: {
+        project_title: "EndlessCode",
+        project_date: "unknown",
+        project_tag: ["website"],
+        project_category: "Website",
+        project_image_thumbnail: "EndlessCode.jpg",
+        project_image_mockup: "endlesscode.png",
+        project_description: "Website untuk mempelajari programming. Dibuat untuk kualifikasi kompetisi OLIVIA 2018 kategori lomba Web Design",
+        project_type: ["Website", "UI Design"],
+        project_technology: {
+            technology_1: {
+                technology_name: "HTML",
+                technology_link: "https://www.google.com/"
+            },
+            technology_2: {
+                technology_name: "CSS",
+                technology_link: ""
+            },
+            technology_3: {
+                technology_name: "javascript",
+                technology_link: ""
+            }
+        },
+        project_team: ["Danis.Z", "Fadli.H"],
+        project_url: {
+            url_name: "Visit Website",
+            url_link: "http://endlesscode.000webhostapp.com/"
+        },
+        project_screenshot: {
+            project_screenshot_1: {
+                project_screenshot_title: "Landing Page",
+                project_screenshot_image: "EndlessCode-1.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_2: {
+                project_screenshot_title: "Features",
+                project_screenshot_image: "EndlessCode-2.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_3: {
+                project_screenshot_title: "Testimonial",
+                project_screenshot_image: "EndlessCode-3.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_4: {
+                project_screenshot_title: "Courses",
+                project_screenshot_image: "EndlessCode-4.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_5: {
+                project_screenshot_title: "Courses Detail",
+                project_screenshot_image: "EndlessCode-5.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_6: {
+                project_screenshot_title: "Login Page",
+                project_screenshot_image: "EndlessCode-6.jpg",
+                project_screenshot_grid: 6
+            },
+        }
+    },
+    key_2: {
+        project_title: "AltaMusic",
+        project_date: "unknown",
+        project_tag: ["website", "uiux"],
+        project_category: "UI Design",
+        project_image_thumbnail: "altamusicc.jpg",
+        project_image_mockup: "altamusic.png",
+        project_description: "Website untuk streaming music online dengan suntuhan style Fluent Design",
+        project_type: ["Website"],
+        project_technology: {
+            technology_1: {
+                technology_name: "HTML",
+                technology_link: "https://www.google.com/"
+            },
+            technology_2: {
+                technology_name: "CSS",
+                technology_link: ""
+            },
+            technology_3: {
+                technology_name: "javascript",
+                technology_link: ""
+            }
+        },
+        project_team: ["DanZai"],
+        project_url: {
+            url_name: "Visit Website",
+            url_link: "http://endlesscode.000webhostapp.com/"
+        },
+        project_screenshot: {
+            project_screenshot_1: {
+                project_screenshot_title: "Header",
+                project_screenshot_image: "AltaMusic-1.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_2: {
+                project_screenshot_title: "Features",
+                project_screenshot_image: "AltaMusic-2.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_3: {
+                project_screenshot_title: "Genre",
+                project_screenshot_image: "AltaMusic-3.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_4: {
+                project_screenshot_title: "Music List with Hover Effect",
+                project_screenshot_image: "AltaMusic-4.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_5: {
+                project_screenshot_title: "Artist",
+                project_screenshot_image: "AltaMusic-5.jpg",
+                project_screenshot_grid: 6
+            },
+            project_screenshot_6: {
+                project_screenshot_title: "Artist Detail",
+                project_screenshot_image: "AltaMusic-6.jpg",
+                project_screenshot_grid: 6
+            },
+        }
+    }
+};
